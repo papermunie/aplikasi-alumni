4d0d16c1005b12872ad5f08719739edb630ba37f
kurasep_ <rizkya7y123@gmail.com>
2022-09-02 09:32:57 +0700

    change
    Signed-off-by: kurasep_ <rizkya7y123@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Konsultasi from './components/Konsultasi/konsultasi'
 import AddTraining from './components/TambahTraining/AddTraining';
 import Sidebar from './components/Sidebar/sidebar';
 import Notifikasi from './components/notifikasi/notifikasi';
+import DetailKonsultasi from './components/Konsultasi/detailKonsultasi';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route  exact path='/AddTraining' element={<AddTraining />} />
         <Route  exact path='/' element={<Notifikasi />} />
         <Route  exact path='/konsultasi' element={<Konsultasi />} />
+        <Route  exact path='/detailKonsultasi' element={<DetailKonsultasi />} />
         </Routes>
       </>
       </Router>
